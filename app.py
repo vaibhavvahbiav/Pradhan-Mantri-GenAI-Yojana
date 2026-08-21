42c7785aab9e87df5ecf5362dc4cb7610dd5f9cf
vaibhavvahbiav <vaibhavatkurwar31@gmail.com>
2026-08-21 15:05:59 +0530

    Day 1

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ llm = OllamaLLM(model="gemma2:2b")
 output_parser = StrOutputParser()
 chain = prompt | llm | output_parser
 if input_text:
-    st.write(chain.invoke({"question": input_text}))
+    st.write(chain.invoke({"question": input_text})) 
